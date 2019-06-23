dd11d39dcc52f93ba7d2f991d7172c70b3e0f0eb
timmens <tim@mensinger96.com>
2019-06-23 16:56:56 +0200

    Updated pre-commit Setup

--- causal_tree/simulate_treatment_data.py
+++ causal_tree/simulate_treatment_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def simulate_treatment_data(agents=100):
+def simulate_treatment_data(agents=100, noise=False):
     x = np.random.normal(0, 1, size=2 * agents).reshape((agents, 2))
     treatment_status = np.random.binomial(1, 0.5, agents)
     treatment_index = np.array(treatment_status, dtype="bool")
@@ -10,18 +10,38 @@ def simulate_treatment_data(agents=100):
 
     y = np.zeros(agents)
 
-    #  index_quadrant1 = index0 * index1
     index_quadrant2 = (~index0) * index1
     index_quadrant3 = (~index0) * (~index1)
     index_quadrant4 = index0 * (~index1)
 
-    y[index_quadrant2 * treatment_index] = 2  # treatment effect = 2
-    y[index_quadrant2 * (~treatment_index)] = 0
+    if not noise:
+        y[index_quadrant2 * treatment_index] = 2
+        y[index_quadrant2 * (~treatment_index)] = 0
 
-    y[index_quadrant3 * treatment_index] = 2  # treatment effect = 1
-    y[index_quadrant3 * (~treatment_index)] = 1
+        y[index_quadrant3 * treatment_index] = 20
+        y[index_quadrant3 * (~treatment_index)] = 6
 
-    y[index_quadrant4 * treatment_index] = -3  # treatment effect = 5
-    y[index_quadrant4 * (~treatment_index)] = 2
+        y[index_quadrant4 * treatment_index] = -10
+        y[index_quadrant4 * (~treatment_index)] = -5
 
-    return x, treatment_status, y
+    else:
+        y[index_quadrant2 * treatment_index] = 2 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant2 * treatment_index)
+        )
+        y[index_quadrant2 * (~treatment_index)] = 0 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant2 * (~treatment_index))
+        )
+        y[index_quadrant3 * treatment_index] = 20 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant3 * treatment_index)
+        )
+        y[index_quadrant3 * (~treatment_index)] = 6 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant3 * (~treatment_index))
+        )
+        y[index_quadrant4 * treatment_index] = -10 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant4 * treatment_index)
+        )
+        y[index_quadrant4 * (~treatment_index)] = -5 + np.random.normal(
+            0, 0.5, size=np.sum(index_quadrant4 * (~treatment_index))
+        )
+
+    return x, y, treatment_status
