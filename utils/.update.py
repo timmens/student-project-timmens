3d36cc3db576966b31a6931071763e09004ce58c
timmens <tim@mensinger96.com>
2019-07-07 12:55:41 +0200

    Updated README.md; cleaned file mess

--- utils/.update.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-""" This script updates all files, including the submodules."""
-import subprocess
-
-subprocess.check_call(["git", "pull"])
-subprocess.check_call(["git", "submodule", "update", "--recursive", "--remote"])
