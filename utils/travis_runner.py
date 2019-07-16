9f7936fe9fdcb6ee9e1878c88218f3fcfd28fff3
timmens <tim@mensinger96.com>
2019-07-16 17:36:49 +0200

    Fixed Bug with travis_runner.py

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     for subdir in subdirectories:
         for notebook in glob.glob(subdir + "/*.ipynb"):
-            cmd1 = "jupyter nbconvert --execute"
+            cmd1 = "jupyter nbconvert --execute "
             cmd2 = "{}  --ExecutePreprocessor.timeout=-1".format(notebook)
             cmd = cmd1 + cmd2
             sp.check_call(cmd, shell=True)
